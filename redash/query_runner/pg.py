13eccd5dd273747671f33e80579b7b4c11885fb1
peterbam <peter.bamberger@pushpay.com>
2024-08-02 09:43:53 +1200

    DATABI-3555 Improve Redash query_limits user notifications

--- redash/query_runner/pg.py
+++ redash/query_runner/pg.py
@@ -264,8 +264,8 @@ class PostgreSQL(BaseSQLQueryRunner):
             if cursor.description is not None:
                 if QUERY_RESULTS_MAX_ROWS != -1 and cursor.rowcount > QUERY_RESULTS_MAX_ROWS:
                     json_data = None
-                    error = "Query returned too many rows ({0}) - maximum allowed rows is {1}".format(
-                        cursor.rowcount, QUERY_RESULTS_MAX_ROWS
+                    error = "Query returned too many rows ({0}) - maximum allowed rows is {1}.<br />See <a href=\"https://pushpay.atlassian.net/wiki/spaces/{2}\" target=\"_blank\">Confluence {2}</a> for more information".format(
+                        cursor.rowcount, QUERY_RESULTS_MAX_ROWS, QUERY_RESULTS_MAX_ROWS_CONFLUENCE
                     )
                 else:
                     columns = self.fetch_columns(
